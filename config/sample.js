197313903296e2a4b4d232ecd31180838337255a
Shizuilab <47310888+shizuilab@users.noreply.github.com>
2020-04-13 08:56:58 +0900

    Update sample.js

--- config/sample.js
+++ config/sample.js
@@ -4,7 +4,7 @@ module.exports = {
     PRIVATEKEY: "123456789....ABCDEF", //送り元の秘密鍵
     ADDRESS: "N............", //送り先のNEMアドレス
     NAMESPACE: "shizui", //ネームスペース
-    MOSAIC: "wanapi", //モザイク名（nemlog記事にリクエストいただければお分けします
+    MOSAIC: "wanapi", //モザイク名（nemlog記事にリクエストいただければお分けします)
     PLACE: "home" //WanaPi!を設置する場所（プレフィックス）
 }
 
