1929ed2f2982f2d9fc915931bb05dc690859b0ef
shizuilab <shizuilab@gmail.com>
2020-04-13 08:56:16 +0900

    added sample.js

---
+++ config/sample.js
@@ -0,0 +1,10 @@
+// sample.jsをwanapi-nis1.jsにリネームしてご使用ください
+
+module.exports = {
+    PRIVATEKEY: "123456789....ABCDEF", //送り元の秘密鍵
+    ADDRESS: "N............", //送り先のNEMアドレス
+    NAMESPACE: "shizui", //ネームスペース
+    MOSAIC: "wanapi", //モザイク名（nemlog記事にリクエストいただければお分けします
+    PLACE: "home" //WanaPi!を設置する場所（プレフィックス）
+}
+
